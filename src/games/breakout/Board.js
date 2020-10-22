1c7a2d6e5b8f0065a9b2937b61e4098b9e2a90f9
Aastha Jain <aasthajain2001.aj@gmail.com>
2020-10-22 22:38:08 +0530

    ball and paddle added

--- src/games/breakout/Board.js
+++ src/games/breakout/Board.js
@@ -1,20 +1,57 @@
 import React, {useRef, useEffect} from 'react';
+import { BallMovement } from './BallMovement';
+import Paddle from './Paddle';
+import WallCollision from "./util/WallCollision";
+import data from '../../data';
+
+
 
 export default function Board()
 {
 	const canvasRef= useRef(null);
 
-	useEffect(
-		()=>{
+	let {ballObj, paddleProps}= data;
+
+	useEffect(()=>{
+		
+		const render= ()=>{
 		const canvas= canvasRef.current;
 		const ctx = canvas.getContext('2d');
 
-		ctx.fillStyle = 'green';
-		ctx.fillRect(10, 10, 150, 100);
+		
 
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		BallMovement(ctx, ballObj);
+
+		// WallCollision(ballObj, canvas);
+
+		if (ballObj.y - ballObj.rad <= 0 || ballObj.y + ballObj.rad >= canvas.height ) {
+    ballObj.dy *= -1;
+  }
+
+  if (ballObj.x + ballObj.rad >= canvas.width || ballObj.x - ballObj.rad <= 0) {
+    ballObj.dx *= -1;
+  }
+
+    Paddle(ctx, canvas, paddleProps);
+
+	requestAnimationFrame(render);
+}
+
+render();
+		
 
 	}, []);
-	return <canvas id="myCanvas" ref= { canvasRef} width="800" height="600" />;
+	return <canvas id="myCanvas" 
+	ref= { canvasRef } 
+	onMouseMove={(event) =>
+          (paddleProps.x =
+            event.clientX -
+            (window.innerWidth < 900 ? 10 : (window.innerWidth * 20) / 200) -
+            paddleProps.width / 2 -
+            10)
+        } width="800" height="600" />;
 
 }
 		
